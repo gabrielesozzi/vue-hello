dff9cd2afce29016b87b3142fa38f8d9d892e567
gabriele <sozziga@gmail.com>
2021-03-23 16:57:15 +0100

    Completato con bonus

---
+++ main.js
@@ -0,0 +1,16 @@
+var root = new Vue({
+el: '#root',
+  data: {
+   title: 'Scegli la destinazione della tua prossima vacanza (lockdown permettendo)',
+   city1: 'Parigi',
+   city2:'Ginevra',
+   city3:'Londra',
+   city4:'Tokyo',
+   classe: 'text',
+   city1_image: 'https://sognandoparigi.imgix.net/wp-content/uploads/2016/05/febbraio-parigi.jpg',
+   city2_image: 'https://www.svizzera.net/wp-content/uploads/sites/6/ginevra.jpg',
+   city3_image:'https://london.ac.uk/sites/default/files/styles/max_1300x1300/public/2018-10/london-aerial-cityscape-river-thames_1.jpg?itok=6LenFxuz',
+   city4_image: 'https://d1ix9yerv4y8lr.cloudfront.net/blog/wp-content/uploads/2019/07/tokyo-ops-7-19.jpg',
+   width: 'img'
+  }
+})
